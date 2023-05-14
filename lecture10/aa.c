aca64f9703a2f3a98d775a3cc5cc20f09e939139
sncohncol <ch0i000@naver.com>
2023-05-14 17:09:19 +0900

    10주차 과제

---
+++ lecture10/aa.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() 
+{
+    char hero[20] = "정현우";
+    char heroin[20] = "박소빈";
+    char boss[20] = "최수정";
+
+    printf("\n\n\n***  난 니가 싫어  ***\n\n\n");
+    printf("%s는 %s에게 고백을 했다...\n",hero,heroin);
+    printf("%s이 현우의 뺨을 쎄게 떄렸다\n",boss);
+    printf("시작은 했는데 수습이 안된다 ...");
+
+    return 0;
+}
